29330c1c1013ebded3e315d425096d97771c9519
wangjialin <wangjialin@foxmail.com>
2017-09-17 15:32:39 +0800

    test2;

---
+++ src/main/java/org/jlhh/mes/repository/UserReposiotry.java
@@ -0,0 +1,10 @@
+package org.jlhh.mes.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by wangjialin on 2017/9/17.
+ */
+@Repository
+public class UserReposiotry {
+}
---
+++ src/main/java/org/jlhh/mes/service/MenuService.java
@@ -0,0 +1,7 @@
+package org.jlhh.mes.service;
+
+/**
+ * Created by wangjialin on 2017/9/17.
+ */
+public class MenuService {
+}
